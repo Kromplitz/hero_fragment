abf09b32f576540e54d7b6eba8c05046c347207b
Kromplias <oles.media@gmail.com>
2024-02-03 12:05:23 +0200

    додано характеристики героя і розмітку

--- app/src/main/java/com/example/list/Game.kt
+++ app/src/main/java/com/example/list/Game.kt
@@ -3,12 +3,21 @@ package com.example.list
 
 data class Hero (
      val name: String,
-     val images:Image)
+     val images:Image,
+     val powerstats: Powerstats
+     )
 data class Image (
 val xs: String,
 val sm: String,
 val md: String,
 val lg: String
 )
+
+data class Powerstats(
+     val intelligence: Long,
+     val strength: Long,
+     val speed: Long,
+     )
+
 
 
